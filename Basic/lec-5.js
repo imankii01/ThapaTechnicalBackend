88b8108285086ec46c1abe88d11b1181ddf4030a
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-25 12:15:36 +0530

    core modules lecture

---
+++ Basic/lec-5.js
@@ -0,0 +1,11 @@
+// Core module->>>>>>
+/*
+consider modules to be the same as js librarie .A set of function you wan tot incluse in your application 
+Nodejs has a set of built in modules which you can use withous installations 
+
+
+
+
+*/
+const fs = require("fs");
+fs.writeFileSync("new.txt", "kajhsdfkajhf");
