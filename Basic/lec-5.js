d358886b76f514ea57b37408a2e22014a4d9e657
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-25 12:27:53 +0530

    core modules

--- Basic/lec-5.js
+++ Basic/lec-5.js
@@ -9,4 +9,8 @@ Nodejs has a set of built in modules which you can use withous installations
 */
 const fs = require("fs");
 // fs.writeFileSync("new.txt", "kajhsdfkajhf");
-fs.appendFileSync("new.txt", "agagagagagag");
+// fs.appendFileSync("new.txt", "agagagagagag");
+
+const buf_data = fs.readFileSync("Basic/new.txt");
+// buffer  ismaiknly used ot store binary data
+console.log(buf_data.toString());
