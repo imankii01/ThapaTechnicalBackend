853cb389ca31cba3eb6955387723dcf93c63a4b9
Ankit <private.ankit047@gmail.com>
2025-01-01 16:48:24 +0530

    Auto-commit at 1/1/2025, 4:48:24 PM

---
+++ dummy-2025-01-01T11-18-24-627Z.js
@@ -0,0 +1,11 @@
+
+// filepath: /Users/ankit/development/simple-git/repo/ThapaTechnicalBackend/dummy-2025-01-01T11-18-24-627Z.js
+function helloWorld() {
+  console.log('Hello, world!');
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { helloWorld, add };
